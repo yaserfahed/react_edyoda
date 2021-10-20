88a98c37856e535ec97f8e9b64bd51bb494eb2cc
Syed Yaser Fahed <86570053+yaserfahed@users.noreply.github.com>
2021-10-20 12:20:01 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Footer from './Footer';
+import Header from './Header';
+import LatestPost from './LatestPost';
+
+
+function App() {
+  return (
+    <div className="App">
+    <Header/>
+      <LatestPost/>
+      <Footer/>
+    
+    </div>
+  );
+}
+
+export default App;
